3b0c10f2c7ecf0e43cdc4b3dba64b07758313890
Valery Remenyuk <remenyuk79@gmail.com>
2020-10-26 13:01:30 +0300

    PostPreviewResponse.class developed by postAnnounce set up. Still "SQLException: Field 'announce' doesn't have a default value" occurs.

--- src/main/java/main/api/response/PostPreviewResponse.java
+++ src/main/java/main/api/response/PostPreviewResponse.java
@@ -1,7 +1,10 @@
 package main.api.response;
 
+import main.entity.Post;
+import main.repository.PostRepository;
 import main.service.PostPreview;
 import main.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import java.sql.Timestamp;
@@ -14,6 +17,22 @@ public class PostPreviewResponse {
     private LocalDate date;
     private Timestamp timestamp;
     private String postAnnounce;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public PostPreviewResponse(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    {
+        try {
+            Post post = postRepository.findById(postId).get();
+            postAnnounce = post.getAnnounce();
+        } catch (NullPointerException ex) {
+            postAnnounce = "No such post, no announce.";
+        }
+    }
 
     private final PostPreview postPreview = new PostPreview(postId, user, date, timestamp, postAnnounce);
 
