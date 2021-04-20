bb242f288dfc9a54a28f7a66869c088368933b5a
Valery Remenyuk <remenyuk79@gmail.com>
2021-04-20 11:28:41 +0300

    Method authRestore used with new yandex.ru mail address, upd 1.

--- src/main/java/main/controllers/EmailController.java
+++ src/main/java/main/controllers/EmailController.java
@@ -2,6 +2,7 @@ package main.controllers;
 
 import main.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,16 @@ public class EmailController {
 
     @RequestMapping(value = "/send")
     public String send() {
-//        authService.
-        return LocalDateTime.now().toString();
-    }
+// адрес который увидит получатель в поле ОТ:
+        String from = "remenyuk.valery@yandex.ru";
+// адрес получателя, куда отправляем письмо (email пользователя)
+        String to = "remenyuk79@gmail.com";
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject("Это тема письма");
+        message.setText("Воу! А это текст письма! Это просто текст, без оформления и HTML!");
+        mailSender.send(message);
+
+        return "Письмо отправлено в " + LocalDateTime.now().toString();    }
 }
